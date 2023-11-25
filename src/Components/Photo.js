11b0ed3c139618777ef1ebff335da8d359cd75b8
Erika Mii <erikajmii135@gmail.com>
2023-11-24 23:01:38 -0600

    debugging

--- src/Components/Photo.js
+++ src/Components/Photo.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from '../images/logo.jpeg';
 import '../css/Input.css';
 
 function Photo() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  // Define the dimensions based on the screen width
+  const photoWidth = windowWidth > 600 ? '55%' : '100%';
+  const photoHeight = windowWidth > 600 ? '120px' : '80px';
+
   return (
     <div className='photo'>
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '55px', marginBottom: '55px'}}>
-      <img
-        src={logo}
-        alt=""
-        style={{ width: '55%', height: '120px' }}
-      />
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '55px', marginBottom: '55px' }}>
+        <img
+          src={logo}
+          alt=""
+          style={{ width: photoWidth, height: photoHeight }}
+        />
       </div>
     </div>
   );

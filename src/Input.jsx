3ce2a659ab7d233c1c4b61e94090f1da60350042
Erika Mii <erikajmii135@gmail.com>
2023-11-24 22:10:51 -0600

    Fixing bugs

--- src/Input.jsx
+++ src/Input.jsx
@@ -52,10 +52,10 @@ function Calculate() {
   const [value6, setValue6] = useState('');
 
   //result state variables
-  //const [result, setResult] = useState('');
+  const [result, setResult] = useState('');
   const [textColor, setTextColor] = useState('result-box');
   const [resultText, setResultText] = useState('');
-  //const {resultColor, setResultColor} = useResultColor();
+  const {resultColor, setResultColor} = useResultColor();
   const [isExpanded, setIsExpanded] = React.useState(false);
 
   // search bar state variables
@@ -204,8 +204,8 @@ function Calculate() {
   const calculate = async () => {
     let resultNum = await two_nodes(coord, value2, value3, value4, value5, value6);
     let resultColor = getColor(resultNum);
-    //setResult(resultColor);
-    //setResultColor(resultColor);
+    setResult(resultColor);
+    setResultColor(resultColor);
 
     let textColor = "";
     let resultText = "";

9abbc6dfa81f132d31673456ebe0b144c14c8f23
Erika Mii <erikajmii135@gmail.com>
2023-12-30 17:04:42 -0600

    Adding Back button

--- src/Components/Acknowledgements.js
+++ src/Components/Acknowledgements.js
@@ -1,10 +1,21 @@
 // Acknowledgements.js
 import React from 'react';
+import { Link } from 'react-router-dom';  
 import '../css/Acknowledgements.css'; 
 
 function Acknowledgements() {
+  const backButtonStyle = {
+    textDecoration: 'none',  
+    padding: '10px 20px',    
+    backgroundColor: '#17699d',  
+    color: '#fff',         
+    borderRadius: '5px',   
+    marginLeft: '5px',
+  };
+
   return (
     <div>
+      <Link to="/" style={backButtonStyle}>Go back</Link>
       <div className="acknowledgement-title">
         <h1> Acknowledgements</h1>
         <h3> The following individuals/entities were instrumental in developing this webpage:</h3>
@@ -16,7 +27,7 @@ function Acknowledgements() {
           <p>Dr. Craig Crandall – University of Texas Southwestern Medical Center and Institute for Exercise and Environmental Medicine – Texas Health Presbyterian Hospital Dallas</p>
           <p>Dr. Federico Tartarini – University of Sydney</p>
           <p>Dr. Ollie Jay – University of Sydney</p>
-          <p>Ms. Amy Action – The Phoenix Society</p>
+          <p>Ms. Amy Acton – The Phoenix Society</p>
           <p>The Phoenix Society</p>
         </div>
       </div>
